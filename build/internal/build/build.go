9972ca3d582fe63e78da55b4d58645209377044f
Kevin Fritz <44335182+kharf@users.noreply.github.com>
2024-10-29 10:15:24 +0100

    chore(deps): update module cuelang.org/go/cmd/cue to v0.10.1 (#667)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- build/internal/build/build.go
+++ build/internal/build/build.go
@@ -39,7 +39,7 @@ func (g apigen) name() string {
 var controllerGenDep = "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.16.4"
 
 // when changed, the renovate customManager has also to be updated.
-var cueDep = "cuelang.org/go/cmd/cue@v0.10.0"
+var cueDep = "cuelang.org/go/cmd/cue@v0.10.1"
 
 func (g apigen) run(ctx context.Context, request stepRequest) (*stepResult, error) {
 	gen := request.container.
