2fdb31838740252add531ac2b8d4cec636483a80
Kevin Fritz <44335182+kharf@users.noreply.github.com>
2025-06-27 08:13:40 +0200

    chore(deps): update module sigs.k8s.io/controller-tools/cmd/controller-gen to v0.18.0 (#965)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- dagger/main.go
+++ dagger/main.go
@@ -79,7 +79,7 @@ func (n *Navecd) kubernetesTestEnv(
 }
 
 // when changed, the renovate customManager has also to be updated.
-var controllerGenDep = "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.17.3"
+var controllerGenDep = "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.18.0"
 
 // when changed, the renovate customManager has also to be updated.
 var cueDep = "cuelang.org/go/cmd/cue@v0.13.1"
